578eba5643ac4f0a48eb90fb51982c5d69ecaa1b
luopengfei <luopengfei@tigerbrokers.com>
2016-01-21 15:54:08 +0800

    ADD:UITabelView

--- Swift_UIKit/Swift_UIKit/ViewController.swift
+++ Swift_UIKit/Swift_UIKit/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tableView : UITableView!
+    let dataArray = ["UIAlertView","UIPickerView"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initTableView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func initTableView() {
+        tableView = UITableView(frame: self.view.bounds, style: .Plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        self.view .addSubview(tableView)
+    }
 }
 
+
+extension ViewController : UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell")
+        if (cell == nil) {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
+        }
+        cell?.textLabel?.text = dataArray[indexPath.row]
+        return cell!;
+    }
+}
+
+extension ViewController : UITableViewDelegate {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        switch indexPath.row {
+        case 0:
+            
+            break
+        default:
+            break
+        }
+    }
+}
